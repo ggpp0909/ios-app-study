727c8cbe05aa9a9c7736c02f5bc48dd5932a699e
장영남 <ggpp0909@naver.com>
2022-11-06 23:55:22 +0900

    조건문

--- 01.playground/Contents.swift
+++ 01.playground/Contents.swift
@@ -1,1 +1,12 @@
+// 다크모드 여부
+var isDarkMode: Bool = false
 
+//if (isDarkMode != true) {
+//    print("다크모드아님")
+//} else {
+//    print("다크모드입니다.")
+//}
+
+var title: String = isDarkMode ? "다크모드 입니다" : "다크모드 아닙니다."
+
+print("title: \(title)")
