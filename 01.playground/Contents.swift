69df4105963d931313c308861acfa0893bce1c48
장영남 <ggpp0909@naver.com>
2022-11-07 17:39:48 +0900

    day 5 옵셔널

--- 01.playground/Contents.swift
+++ 01.playground/Contents.swift
@@ -79,26 +79,77 @@
 // ***** day04 ***** 반복문
 // *****************
 
-// range
-// 0...5
-// 0, 1, 2, 3, 4, 5
+//// range
+//// 0...5
+//// 0, 1, 2, 3, 4, 5
+//
+//// 0..<5
+//// 0, 1, 2, 3, 4
+//for i in 0...5 {
+//    print("i: \(i)")
+//}
+//
+//for i in 0..<5 where i % 2 == 0{
+//    print("i: \(i)")
+//}
+//
+////var randomInts: [Int] = []
+//var randomInts: [Int] = [Int]()
+//
+//for _ in 0..<25 {
+//    let randomNumber = Int.random(in: 0...100)
+//    randomInts.append(randomNumber)
+//}
+//
+//print("randomInts: \(randomInts)")
 
-// 0..<5
-// 0, 1, 2, 3, 4
-for i in 0...5 {
-    print("i: \(i)")
+
+// *****************
+// ***** day05 ***** 옵셔널
+// *****************
+
+// 옵셔널이란?
+// 값이 있는지 없는지 모른다.
+// 값이 없다는 것을 nil이라고 함
+var someVariable: Int? = nil
+
+if someVariable == nil {
+    someVariable = 90
 }
 
-for i in 0..<5 where i % 2 == 0{
-    print("i: \(i)")
+// 업랩핑이란? 랩 즉 감싸져있는 것을 벗기는 것 (optional로 감싸져 있음)
+
+// someVariable에 값이 있다면 otherVariable에 넣어서 쓰겠습니다.
+if let otherVariable = someVariable {
+    // 이렇게 하면 otherVariable은 옵셔널이 아니게 됨(언랩핑됨)
+    print("언랩핑 되었다. 즉 값이 있다. otherVariable: \(otherVariable)")
+} else {
+    // 값이 없을때는 여기로 옴
+    print("값이 없다.")
 }
 
-//var randomInts: [Int] = []
-var randomInts: [Int] = [Int]()
+someVariable = nil
 
-for _ in 0..<25 {
-    let randomNumber = Int.random(in: 0...100)
-    randomInts.append(randomNumber)
+// someVarialbe에 값이 없다면 기본값으로 요놈을 넣겠다.
+let myValue = someVariable ?? 10
+print("myValue \(myValue)")
+
+var firstValue: Int? = 30
+var secondValue: Int? = 50
+print("firstValue \(firstValue)")
+print("secondValue \(secondValue)")
+
+unwrap(firstValue)
+unwrap(secondValue)
+unwrap(nil)
+
+// parameter: 이런식으로 넣는게 싫으면 언더바를 추가
+func unwrap(_ parameter: Int?){
+    print("upwrap() called")
+    // 값이 없으면 리턴 해버린다. if let이랑 거의 똑같음
+    guard let unWrappedParam = parameter else {
+        return
+    }
+    print("unWrappedParam: \(unWrappedParam)")
 }
 
-print("randomInts: \(randomInts)")
